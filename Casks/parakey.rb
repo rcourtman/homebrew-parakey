0e61fe32b787c85f725fd65a278cbd58ea9682dd
Richard Courtman <rcourtman@RICHARD-MAC-MINI.local>
2026-06-10 14:35:46 +0100

    parakey 0.2.16

--- Casks/parakey.rb
+++ Casks/parakey.rb
@@ -1,6 +1,6 @@
 cask "parakey" do
-  version "0.2.15"
-  sha256 "5aa9bed50a1713bd60b56d354835a0f4bad96d3fe21b1b15fe2229a432be2f7b"
+  version "0.2.16"
+  sha256 "13d23e7d424313eb6c4750629f4fb2725b41f2380f3c771834ef000d803d670e"
 
   url "https://github.com/rcourtman/parakey/releases/download/v#{version}/Parakey.zip"
   name "Parakey"
